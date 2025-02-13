221da30d0a0e58040282d8c4b07549d5af5017f8
cmeyer0121 <cmeyer0121@gmail.com>
2025-02-12 23:07:28 -0600

    file parsing for graphing/motor characteristics

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+import json
 import dearpygui.dearpygui as dpg
+from scipy import integrate
+
+# NAMED CONSTANTS FOR CONVERSIONS, maybe move in future?
+TRANSDUCERMINVOLTAGE = 0.5
+TRANSDUCERMAXVOLTAGE = 4.5
+TRANSDUCERMAXPRESSURE = 1600 #In PSI
+TRANSDUCERSCALINGFACTOR = TRANSDUCERMAXPRESSURE / (TRANSDUCERMAXVOLTAGE-TRANSDUCERMINVOLTAGE)
+
+FILENAME = './mock_data.json' # TODO: Add dynamic file pathing for imported JSON files (see read_data() function)
 
 # -------------------------
 # Callback stubs
@@ -17,20 +27,44 @@ def populate_graphs_callback():
     In a real app, you would load data from the uploaded file,
     parse it, and fill in the plot series and any camera/video feed.
     """
+
+    # Our lists to be used in graphing UI and motor characteristics/key stats
+    time, thrusts, pressures = read_data()
+
+    # Calculate key stats/motor characteristics
+    burn_time = time[len(time)-1]
+    avg_thrust = sum(thrusts) / len(thrusts)
+    avg_pressure = sum(pressures) / len(pressures)
+    max_thrust = max(thrusts)
+    max_pressure = max(pressures)
+    total_impulse = integrate.simpson(thrusts,x=time)
+    motor_class = determine_motor_class(total_impulse)
+
     # For demonstration, just show some dummy lines on the plots
     dpg.set_item_label("thrust_series", "Thrust Data")
     dpg.set_item_label("pressure_series", "Pressure Data")
     
     # Thrust data (time vs thrust)
-    time_data     = [0, 1, 2, 3, 4, 5]
-    thrust_values = [0, 1000, 1800, 2000, 1950, 1500]
-    dpg.set_value("thrust_series", [time_data, thrust_values])
+    dpg.set_value("thrust_series", [time, thrusts])
     
     # Pressure data (time vs pressure)
-    pressure_values = [100, 500, 650, 800, 830, 700]
-    dpg.set_value("pressure_series", [time_data, pressure_values])
+    dpg.set_value("pressure_series", [time, pressures])
     
-    # You could also update the “average thrust,” etc. labels here.
+    # Update the “average thrust,” etc. labels here.
+    dpg.set_value("avg_thrust", "Average Thrust: " + '{0:,.2f}'.format(avg_thrust) + " N")
+    dpg.set_value("max_thrust", "Max Thrust: " + '{0:,.2f}'.format(max_thrust) + " N")
+    dpg.set_value("avg_pressure", "Average Pressure: " + '{0:,.2f}'.format(avg_pressure) + " PSI")
+    dpg.set_value("max_pressure", "Max Pressure: " + '{0:,.2f}'.format(max_pressure) + " PSI")
+    dpg.set_value("burn_time", "Burn Time: " + '{0:.2f}'.format(burn_time) + " s")
+    dpg.set_value("total_impulse", "Total Impulse: " + '{0:.2f}'.format(total_impulse) + " Ns")
+    dpg.set_value("motor_desig", "Motor Designation: " + motor_class + '{0:.0f}'.format(avg_thrust))
+
+    # Resize the axes to fit the file data
+    dpg.fit_axis_data("y_axis_thrust")
+    dpg.fit_axis_data("y_axis_pressure")
+
+    dpg.fit_axis_data("x_axis_thrust")
+    dpg.fit_axis_data("x_axis_pressure")
 
 def exit_callback():
     """
@@ -47,6 +81,7 @@ def build_ui():
     """
     Build the entire UI layout inside the primary window.
     """
+
     # Main menu bar across the top
     with dpg.menu_bar():
         # Just top-level items for simplicity
@@ -94,14 +129,14 @@ def build_ui():
                         dpg.add_line_series([], [], label="Pressure Data", tag="pressure_series")
             
             # Key stats at the bottom
-            with dpg.child_window(width=-1, height=80):
-                dpg.add_text("Average Thrust: 1,800.70 N")
-                dpg.add_text("Max Thrust: 2,857.59 N")
-                dpg.add_text("Average Pressure: 549.67 PSI")
-                dpg.add_text("Max Pressure: 829.81 PSI")
-                dpg.add_text("Motor Designation: M1801")
-                dpg.add_text("Burn Time: 5.56 s")
-                dpg.add_text("Total Impulse: 10064.17 Ns")
+            with dpg.child_window(width=-1, height=180):
+                dpg.add_text("Average Thrust: " + " N", tag="avg_thrust")
+                dpg.add_text("Max Thrust: " + " N", tag = "max_thrust")
+                dpg.add_text("Average Pressure: " + " PSI", tag="avg_pressure")
+                dpg.add_text("Max Pressure: " + " PSI", tag="max_pressure")
+                dpg.add_text("Burn Time: " + " s", tag="burn_time")
+                dpg.add_text("Total Impulse: " + " Ns", tag="total_impulse")
+                dpg.add_text("Motor Designation: ", tag="motor_desig")
     
     dpg.add_spacer(height=10)
     
@@ -113,6 +148,91 @@ def build_ui():
     with dpg.child_window(width=-1, height=200):
         # For demonstration, we just show a button with some text or a placeholder image
         dpg.add_button(label="Play Video (Placeholder)")
+
+
+# -------------------------
+# Graphing/crunching imported data
+# -------------------------
+
+# Determines the motor class dependent on the specified impulse
+def determine_motor_class(impulse):
+  if impulse <= 2.5:
+    return 'A'
+  elif impulse <= 5:
+    return 'B'
+  elif impulse <= 10:
+    return 'C'
+  elif impulse <= 20:
+    return 'D'
+  elif impulse <= 40:
+    return 'E'
+  elif impulse <= 80:
+    return 'F'
+  elif impulse <= 160:
+    return 'G'
+  elif impulse <= 320:
+    return 'H'
+  elif impulse <= 640:
+    return 'I'
+  elif impulse <= 1280:
+    return 'J'
+  elif impulse <= 2560:
+    return 'K'
+  elif impulse <= 5120:
+    return 'L'
+  elif impulse <= 10240:
+    return 'M'
+  elif impulse <= 20480:
+    return 'N'
+  elif impulse <= 40960:
+    return 'O'
+  elif impulse <= 81920:
+    return 'P'
+  return ""
+
+# Main function to parse JSON for graphing purposes, translates imported voltages into thrust/pressure values
+def read_data():
+    # Open/point to the JSON file (input file to generate data from)
+    f = open(FILENAME, 'r')
+    data = json.load(f)
+
+    # Declare lists
+    loads = []
+    pressures = []
+    time = []
+
+    # NOTE: Instead of averaging every X samples into one data entry, this is treating each sample as its own entry
+
+    # Read and convert load_cell data into N
+    for lv in data['load_cell_voltages_mv']:
+       loadVoltage = lv
+       #1.25 and 201 taken from dip switches active on the LJ-Tick-InAmp. For more info see https://labjack.com/pages/support?doc=/datasheets/accessories/ljtick-inamp-datasheet/
+       loadAdjVoltage = (loadVoltage - 1.25) / 201
+       #our load cell's senitivity is 2 mV/V
+       #found by calculating the slope and intercept between two known weights and the adjusted voltage
+       calibratedLoad = 100387.5 * loadAdjVoltage - 3.8069375
+       #calibration value is set in kg, to get the reading in N, need to multiply by 9.81
+       calibratedLoad = calibratedLoad * 9.81
+       loads.append(calibratedLoad)
+
+    # Read and convert transducer data into psi
+    for pv in data['pressure_transducer_voltages_v']:
+       pressVoltage = pv #/ num_samples
+       pressAdjVoltage = pressVoltage - TRANSDUCERMINVOLTAGE
+       pressure = pressAdjVoltage * TRANSDUCERSCALINGFACTOR
+       pressures.append(pressure)
+       
+    # Until we have a better input file format, this is the best way to get the number of samples taken
+    n_press_samples = len(pressures)
+    n_ld_samples = len(loads)
+    n_samples = min(n_ld_samples, n_press_samples) # OR take max()
+    sample_rate = data['sample_rate']
+
+    # Gets time array to "sync" data entries to graphs dependent on sampling rate and samples relative to a second
+    for i in range(n_samples):
+        time.append( round( (float) ((i+1) * (float) (1/sample_rate)), 3) )
+
+    return (time, pressures, loads)
 
 # -------------------------
 # Main script
