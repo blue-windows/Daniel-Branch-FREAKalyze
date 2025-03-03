5fa4d8f24f32930ce47ac48f50342dc0f8dd3734
Daniel Thoenen <107645495+blue-windows@users.noreply.github.com>
2025-03-03 00:01:01 -0600

    setup.py will not try to set up a virtual environment if one already exists

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 import platform
 import subprocess
+from pathlib import Path
 
 operating_system = platform.system()
 
 if operating_system == "Windows":
     print(">> python3 -m ensurepip")
     subprocess.call("python3 -m ensurepip", shell=True)
-    print("\n\n>> python3 -m venv ../FREAKalyze")
-    subprocess.call("python3 -m venv ../FREAKalyze", shell=True)
+    if not Path('/Lib').exists():
+        print("\n\n>> python3 -m venv ../FREAKalyze")
+        subprocess.call("python3 -m venv ../FREAKalyze", shell=True)
     print("\n\n>> ./Scripts/activate")
     subprocess.run(["powershell", "-Command", "Start-Process ./Scripts/activate"])
     print("\n\n>> pip install -r ./windows_requirements.txt")
@@ -15,8 +17,9 @@ if operating_system == "Windows":
 else:
     print(">> python3 -m ensurepip")
     subprocess.call("python3 -m ensurepip", shell=True)
-    print("\n\n>> python3 -m venv ../FREAKalyze")
-    subprocess.call("python3 -m venv ../FREAKalyze", shell=True)
+    if not Path('/bin').exists():
+        print("\n\n>> python3 -m venv ../FREAKalyze")
+        subprocess.call("python3 -m venv ../FREAKalyze", shell=True)
     print("\n\n>> source ./bin/activate")
     subprocess.call("source ./bin/activate", shell=True)
     print("\n\n>> pip install -r ./unix_mac_requirements.txt")
