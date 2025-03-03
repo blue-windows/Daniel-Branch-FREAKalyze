8d5321c35c98b1c2329a7ee04c0ee266ab3633e6
Daniel Thoenen <107645495+blue-windows@users.noreply.github.com>
2025-03-02 20:52:46 -0600

    minor change to main.py

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ if platform.system() is "Windows":
    print(">> python3 windows.py")
    subprocess.call("python3 windows.py", shell=True)
 else:
-   print(">> python3 windows.py")
+   print(">> python3 unix_mac.py")
    subprocess.call("python3 unix_mac.py", shell=True)
 
